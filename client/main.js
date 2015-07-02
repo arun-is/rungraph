fde7a55cd557f3bfc193c2ffe4bd3737a9058388
Arun Venkatesan <dallasarun@gmail.com>
2015-07-01 22:17:21 -0700

    Progress percentage, renamed main.js
    -added a simple template to show the download progress
    -renamed main.js to server.js

--- client/main.js
+++ client/main.js
@@ -5,3 +5,20 @@ Meteor.startup(function() {
 Deps.autorun(function(){
   Meteor.subscribe('userData');
 });
+
+Template.progress.helpers({
+    percentage: function() {
+        var user = Meteor.user();
+        console.log(user);
+        if(user && !testNullUndefined(user.savedActivities) && !testNullUndefined(user.totalActivities)) {
+            return (user.totalActivities === 0 ? 0 : ((user.savedActivities / user.totalActivities) * 100).toFixed(0)) + '%';
+        } else {
+            console.error('what is going on?');
+            return null;
+        }
+    }
+});
+
+function testNullUndefined(number) {
+    return number === null || number === undefined;
+}
